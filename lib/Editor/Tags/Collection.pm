d59f5826a47cef9e395fff8406b8a4f71538818b
Jonathan Rockway <jon@jrock.us>
2010-01-06 02:32:53 -0700

    use Set::Object for tags storage instead of array

--- lib/Editor/Tags/Collection.pm
+++ lib/Editor/Tags/Collection.pm
@@ -5,21 +5,27 @@ role Editor::Tags::Collection {
     use Editor::Tags::Types qw(Tag);
     use MooseX::Types::Moose qw(ArrayRef HashRef);
 
-    has 'tags' => (
-        traits     => ['Array'],
+    use Set::Object;
+
+    has '_tags' => (
         is         => 'ro',
-        isa        => ArrayRef[Tag],
+        isa        => 'Set::Object',
         required   => 1,
-        default    => sub { +[] },
-        auto_deref => 1,
+        default    => sub { Set::Object->new },
         trigger    => sub {
             my $self = shift;
             $self->_clear_file_tag_map;
         },
         handles => {
-            add_tag => 'push',
+            add_tag    => 'insert',
+            remove_tag => 'delete',
+            tags       => 'members',
         },
     );
+
+    before add_tag(Tag $tag){
+        return; # only here for type validation
+    }
 
     has 'file_tag_map' => (
         init_arg   => undef,
@@ -45,5 +51,9 @@ role Editor::Tags::Collection {
 
     method add_tags(@tags){
         $self->add_tag($_) for @tags;
+    }
+
+    method forget_file(Str $filename){
+        $self->remove_tag($_) for @{$self->get_file_tags($filename)};
     }
 }
