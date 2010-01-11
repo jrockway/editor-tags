453acb273f5a322ad6be0875356d0bf55f9b6bf6
Jonathan Rockway <jon@jrock.us>
2010-01-11 17:27:42 -0600

    add tag kind spec to exuberant-ctags output

--- lib/Editor/Tags/File/ExuberantCTags.pm
+++ lib/Editor/Tags/File/ExuberantCTags.pm
@@ -3,11 +3,27 @@ use MooseX::Declare;
 class Editor::Tags::File::ExuberantCTags extends Editor::Tags::File::CTags {
     use Editor::Tags::Types qw(Tag);
     use Editor::Tags::Tag;
-    
+
+    method build_extra_info(ClassName|Object $class: Tag $tag){
+        my %extra_info = %{$tag->extra_info};
+        $extra_info{line} = $tag->line if $tag->line;
+
+        my $kind = 's';
+        $kind = 'p' if $extra_info{kind} && $extra_info{kind} =~ /package/;
+
+        delete $extra_info{kind}; # special case.
+        return ($kind, \%extra_info);
+    }
+
     override build_formatted_tag(ClassName|Object $class: Tag $tag) {
         my $pattern = $tag->address_pattern;
-        my $result = join "\t", $tag->name, $tag->associated_file, qq{$pattern;"},
-          map { join ':', $_, $tag->extra_info->{$_} } grep { $tag->extra_info->{$_} } keys %{$tag->extra_info};
+        my ($kind, $extra_info) = $class->build_extra_info($tag);
+
+        my $result = join "\t", $tag->name, $tag->associated_file,
+          qq{$pattern;"}, $kind,
+          map { join ':', $_, $extra_info->{$_} }
+            grep { $extra_info->{$_} }
+              keys %$extra_info;
         return "$result\n";
     }
 }
