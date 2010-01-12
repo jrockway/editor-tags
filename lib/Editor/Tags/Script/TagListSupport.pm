c1dfa88c7d5150e889a6636a916c1b678763807e
Jonathan Rockway <jon@jrock.us>
2010-01-12 03:57:55 -0600

    ok, so i should really get my blog working again...

--- lib/Editor/Tags/Script/TagListSupport.pm
+++ lib/Editor/Tags/Script/TagListSupport.pm
@@ -1,16 +1,28 @@
 use MooseX::Declare;
 
-# TagList is a Vim plugin that claims to support ctags, but does not
-# actually understand the content.  It also can't read tags from a
-# file; only from stdin.  So this app runs in the background, accepts
-# a request (via pclient) for a file's tags, and prints the result.
-# (Oh, and if the tags aren't in exactly the right format, TagList
-# totally ignores the tag.  Because it's fucking dumb.)
+# TagList is a Vim plugin that turns ctags data into a nice menu, but
+# does, unfortunately, not actually understand the content of ctags
+# files.  Oh, it also can't read tags from a *file*; only from stdin.
+# It also can't read a tags file that has tags for more than one file.
+# (You keep a tags file for each file in your project, riiiight?)
 #
-# Emacs' Speedbar can handle a tag file for your entire project, but
-# that's because smart people use Emacs.  (It also doesn't whine when
-# the TAGS file is changed out from under it; it just silently uses
-# the new data.  It's like someone with a brain designed it...)
+# So this app runs in the background, accepts a request (via pclient)
+# for a file's tags, and prints them, in exactly the format taglist
+# desires, to stdout.  (Did I mention that if the tags aren't in
+# exactly the right format, with exactly the right metadata and only
+# the right metadata, TagList totally ignores the tag?  Oh, well... it
+# does.  Because vim can't parse files whose format specification
+# document is included in the fucking vim source code.)
+#
+# I will mention that Emacs' Speedbar can handle a TAGS file for your
+# entire project (or, get this, multiple TAGS files), but that's
+# because smart people use Emacs.  It also doesn't whine when the TAGS
+# file is changed out from under it; it just silently uses the new
+# data.  It's like someone with a brain designed it...
+#
+# For maximum enjoyment; use Emacs and the Script::Update instead of
+# this.  It's nicer, faster, and more user-friendly.  And you have
+# Lisp.
 
 class Editor::Tags::Script::TagListSupport
   with (MooseX::Runnable, MooseX::Getopt) {
